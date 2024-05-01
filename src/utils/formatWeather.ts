b76a1ce0896de4541f99292efb45411d2d38eb8d
arthur <arthurdesouzareis222@gmail.com>
2024-05-01 05:47:44 -0300

    chore: formatar dados da api

---
+++ src/utils/formatWeather.ts
@@ -0,0 +1,22 @@
+import { ForecastCurrentData } from '../types/forecast';
+
+export const FormatWeather = (data: any): ForecastCurrentData => {
+  return {
+    name: data.location.name,
+    region: data.location.region,
+    country: data.location.country,
+    temp_c: data.current.temp_c,
+    temp_f: data.current.temp_f,
+    is_day: data.current.is_day,
+    last_updated: data.current.last_updated,
+    wind_degree: data.current.wind_degree,
+    wind_kph: data.current.wind_kph,
+    pressure_mb: data.current.pressure_mb,
+    pressure_in: data.current.pressure_in,
+    precip_mm: data.current.precip_mm,
+    precip_in: data.current.precip_in,
+    humidity: data.current.humidity,
+    feelslike_c: data.current.feelslike_c,
+    feelslike_f: data.current.feelslike_f,
+  };
+};

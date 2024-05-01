773abbefac6e968270e393af9e18a6384bf83cbe
arthur <arthurdesouzareis222@gmail.com>
2024-05-01 08:11:28 -0300

    chore: padronizar parametros parametros da req

--- src/schemas/forecast.ts
+++ src/schemas/forecast.ts
@@ -1,5 +1,12 @@
 import { z } from 'zod';
 
 export const ForecastSchema = z.object({
-  city: z.string(),
+  city: z.string().transform((name) => {
+    return name
+      .trim()
+      .split(' ')
+      .map((word) => {
+        return word[0].toLocaleUpperCase().concat(word.substring(1));
+      }).join(" ");
+  }),
 });

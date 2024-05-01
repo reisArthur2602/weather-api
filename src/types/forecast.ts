f34b538ca03f7e0124a53132b514586745d71238
arthur <arthurdesouzareis222@gmail.com>
2024-05-01 05:52:27 -0300

    chore: tipar retorno da API formatada

---
+++ src/types/forecast.ts
@@ -0,0 +1,7 @@
+import { Prisma } from '@prisma/client';
+import { prismaClient } from '../database/prisma';
+
+export type ForecastCurrentData = Prisma.Args<
+  typeof prismaClient.forecastCurrent,
+  'create'
+>['data'];

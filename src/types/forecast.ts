21742a4c50b495db57b7d98ecf5defc19e7c5c8a
arthur <arthurdesouzareis222@gmail.com>
2024-05-02 07:04:24 -0300

    refactor: refatorar model  e types

--- src/types/forecast.ts
+++ src/types/forecast.ts
@@ -5,3 +5,13 @@ export type ForecastCurrentData = Prisma.Args<
   typeof prismaClient.forecastCurrent,
   'create'
 >['data'];
+
+// export type UpdateForecastProps = {
+//   id: string;
+//   maxtemp_c: number;
+//   maxtemp_f: number;
+//   mintemp_c: number;
+//   mintemp_f: number;
+//   avgtemp_c: number;
+//   avgtemp_f: number;
+// };

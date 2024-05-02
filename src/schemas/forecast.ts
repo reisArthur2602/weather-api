4ca8e2ce5fc9ce7219f0882c870a86efe7d74e32
arthur <arthurdesouzareis222@gmail.com>
2024-05-02 15:41:31 -0300

    feat: criar filterSchema e formatDate

--- src/schemas/forecast.ts
+++ src/schemas/forecast.ts
@@ -1,12 +1,17 @@
 import { z } from 'zod';
+import { formatDate } from '../utils/Date';
 
-export const ForecastSchema = z.object({
-  city: z.string().transform((name) => {
-    return name
-      .trim()
-      .split(' ')
-      .map((word) => {
-        return word.toLocaleLowerCase()
-      }).join(" ");
-  }),
+export const CitySchema = z.object({
+  city: z
+    .string()
+    .trim()
+    .transform((city) => {
+      return city.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
+    }),
+});
+
+export const FilterSchema = z.object({
+  city: z.string().transform((city) => city.toLowerCase()),
+  startDate: z.date().transform((date) => formatDate(date)),
+  endDate: z.date().transform((date) => formatDate(date)),
 });
---
+++ src/utils/Date.ts
@@ -0,0 +1,15 @@
+export const getCurrentDate = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+export const formatDate = (date: Date): string  => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+};
+

48b810f3f34043d67d00104cbeb5c4ed0a6bb38e
arthur <arthurdesouzareis222@gmail.com>
2024-05-15 19:38:14 -0300

    feat: install e config swagger

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import * as weather from './controllers/forecast';
 export const router = Router();
 
-router.get('/weather', weather.getCityByFilter);
+router.get('/weather/filter', weather.getCityByFilter);
 router.get('/weather/:city', weather.getByCity);
-router.post('/weather', weather.searchCity);
+router.post('/weather/search', weather.searchCity);
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { router } from './routes';
-
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () =>
   console.log(`🚀 Server rodando na porta ${process.env.PORT}`)
